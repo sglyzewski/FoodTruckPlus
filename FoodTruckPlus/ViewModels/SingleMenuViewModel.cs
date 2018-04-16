6814c7ba6292cc85eae7ef33825abeedb792f7f4
Stephanie Glyzewski <stephanie.glyzewski@gmail.com>
2018-04-16 08:47:13 -0500

    just an update

---
+++ FoodTruckPlus/ViewModels/SingleMenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FoodTruckPlus.Models;
+
+namespace FoodTruckPlus.ViewModels
+{
+    public class SingleMenuViewModel
+    {
+        public FullMenu FullMenu { get; set; }
+        public IEnumerable<MenuItem> MenuItemsForFullMenu {get; set;}
+    }
+}
